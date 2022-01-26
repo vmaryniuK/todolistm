9a28e6963fd526ddade3e4764c710a9a4e92fd07
John Doe <mr.martunyuk@gmail.com>
2022-01-26 20:44:41 +0200

    improve addtem

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,30 +3,38 @@ import "./item-add-form.css"
 
  class ItemAddForm extends Component{
 
-   state=({
+   state={
        label:" "
-   })
+   };
    onLabelChange=(e)=>{
     this.setState({
         label:e.target.value
     })
-   }
+   };
    onSubmit=(e)=>{
        e.preventDefault();
-       this.props.AddItem( this.state.label )
+       this.props.AddItem(this.state.label);
+         
+               this.setState({
+               label:" "
+           });
+       
    }
     render(){
         
         
     return(
         <form className="item-add-form"
-        onSubmit={this.onSubmit}>
+            onSubmit={this.onSubmit}>
+
             <input type="text" 
             className="form-control"
+            onChange={this.onLabelChange}
             placeholder="What we need to do"
-            onChange={this.onLabelChange}/>
-            <button className="btn btn-outline-secondary"
-            >
+            
+            value={this.state.label}/>
+
+            <button className="btn btn-outline-secondary">
                 Add Item
             </button>
         </form>
